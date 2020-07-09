6b869ad8c5838e60e0865240443463a46c851b53
lee myung ho <dl_audgh@naver.com>
2020-07-09 22:26:05 +0900

    end

---
+++ 1주차/Boj1000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Boj1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("a , b정수를 입력해주세요");
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+}
