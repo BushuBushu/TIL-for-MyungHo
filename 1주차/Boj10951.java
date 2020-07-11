a6091e76657f15699da4a1d8e7f9ff75231f81c9
lee myung ho <dl_audgh@naver.com>
2020-07-12 00:25:32 +0900

    1주차 4일 백준10951

---
+++ 1주차/Boj10951.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Boj10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int a,b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a + b);
+		}
+
+	}
+}
