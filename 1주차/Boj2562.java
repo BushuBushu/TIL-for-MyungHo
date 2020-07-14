3f22fb130a048e04dd8d6e6e7d2beef0f093a5f3
lee myung ho <dl_audgh@naver.com>
2020-07-14 21:18:42 +0900

    백준 2562번 최댓값

---
+++ 1주차/Boj2562.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Boj2562 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int max = 0;
+		int index = 0;
+		
+		int [] array = new int[9];
+		
+		for(int i = 0; i<array.length; i++) {
+			array[i] = sc.nextInt();
+			if(array[i] > max) {
+				max = array[i];
+				index = i + 1;
+			}
+		}
+		
+
+		System.out.println(max);
+		System.out.println(index);
+	}
+}
