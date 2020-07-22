0a5296b8cbe581810758920e036bc1504a7249c8
lee myung ho <dl_audgh@naver.com>
2020-07-22 21:54:10 +0900

    07/22 test

---
+++ 2주차/2week/appClientModule/ThisIsJava134_3.java
@@ -0,0 +1,11 @@
+public class ThisIsJava134_3 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i =0; i<=100; i++) {
+			if(i % 3 == 0) {
+				sum+= i;
+			}
+		}
+		System.out.println("3의 배수의 합 : " + sum);
+	}
+}
